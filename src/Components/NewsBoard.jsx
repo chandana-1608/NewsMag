74c1d1a45dd1a8609b39f275894442a201dd4144
Lakshmichandanamarthala <lakshmichandanamarthala@gmail.com>
2025-12-24 09:41:12 +0530

    m

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -4,7 +4,7 @@ import NewsItem from "./NewsItem";
 const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+    let url = `https://newsapi.org/v2/top-headlines?us&category=${category}&apiKey=${
       import.meta.env.VITE_API_KEY
     }`;
     fetch(url)
